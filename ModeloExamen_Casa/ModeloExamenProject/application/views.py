aa281da96cce9236970b450246c43da3f8bfd1a9
liazz03 <lia.castanneda@estudiante.uam.es>
2023-05-11 19:58:28 +0200

    todo completo

--- ModeloExamen_Casa/ModeloExamenProject/application/views.py
+++ ModeloExamen_Casa/ModeloExamenProject/application/views.py
@@ -1,25 +1,35 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 
 #added imports
-from django.shortcuts import get_object_or_404
 from django.views import generic
 from .models import Channel, Suscription
 # Create your views here.
 
-class ChannelView(generic.DetailView):
+class ChannelView(generic.ListView):
+
     model = Channel
     template_name = 'channel.html' 
-    
+    context_object_name = 'subscriptions'
+
+    def get_queryset(self):
+        subscriptions = []
+
+        channell = Channel.objects.filter(id=self.kwargs['pk']).first()
+
+        if channell is not None :
+            subscriptions = Suscription.objects.filter(channel=channell)
+        
+        return subscriptions
+
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        channelobj = self.get_object()
+        channel = Channel.objects.filter(id = self.kwargs['pk']).first()
 
-        context['subscriptions'] = Suscription.objects.filter(channel = channelobj)
-        context['channelName'] = channelobj.channelName
-        #TODO: return error through error variable
+        if channel is None:
+            context['error'] = f"No channels found with pk: {self.kwargs['pk']}" 
+            return context 
+        
         return context
 
-    def get_object(self):
-        pk_ = self.kwargs.get("pk")
-        return get_object_or_404(Channel, id=pk_)
